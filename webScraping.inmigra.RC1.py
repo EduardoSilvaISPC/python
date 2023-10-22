738c408c0cc3b61a17a07d4d181b3e485dfdfc15
EduardoSilvaISPC <107503086+EduardoSilvaISPC@users.noreply.github.com>
2023-10-22 00:12:29 -0300

    Add files via upload

--- webScraping.inmigra.RC1.py
+++ webScraping.inmigra.RC1.py
@@ -8,7 +8,7 @@ import mysql.connector
 ruta_completa = r'C:\Users\delfe\Desktop\BSoup\scrap'
 
 # URL de la página
-url = 'https://datosmacro.expansion.com/demografia/migracion/emigracion/argentina'
+url = 'https://datosmacro.expansion.com/demografia/migracion/inmigracion/argentina'
 
 # Realizar la solicitud GET
 response = requests.get(url)
@@ -23,9 +23,9 @@ table = soup.find('table', class_='tabledat')
 if table:
     # Inicializar listas para almacenar los datos
     years = []
-    emigration_men = []
-    emigration_women = []
-    total_emigration = []
+    inmigration_men = []
+    inmigration_women = []
+    total_inmigration = []
 
     # Encontrar todas las filas de datos en la tabla
     rows = table.find_all('tr')
@@ -35,37 +35,37 @@ if table:
         # Encontrar todas las columnas de datos en la fila
         columns = row.find_all('td')
 
-        # Extraer los datos y almacenarlos
+        # Extraer los datos y almacenarlos en las listas correspondientes
         year = columns[0].text.strip()
         men = columns[1].text.strip().replace('.', '')
         women = columns[2].text.strip().replace('.', '')
         total = columns[3].text.strip().replace('.', '')
 
         years.append(year)
-        emigration_men.append(men)
-        emigration_women.append(women)
-        total_emigration.append(total)
+        inmigration_men.append(men)
+        inmigration_women.append(women)
+        total_inmigration.append(total)
 
-    # Archivo CSV
-    csv_filename = os.path.join(ruta_completa, 'argentina_emigrantes_totales_tabla1.csv')
+    # Crear archivo CSV 
+    csv_filename = os.path.join(ruta_completa, 'argentina_inmigrantes_totales_tabla1.csv')
     with open(csv_filename, 'w', newline='', encoding='utf-8') as csv_file:
-        fieldnames = ['Year', 'Emigration Men', 'Emigration Women', 'Total Emigration']
+        fieldnames = ['Year', 'Inmigration Men', 'Inmigration Women', 'Total Inmigration']
         writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
         writer.writeheader()
 
         for i in range(len(years)):
             writer.writerow({
                 'Year': years[i],
-                'Emigration Men': emigration_men[i],
-                'Emigration Women': emigration_women[i],
-                'Total Emigration': total_emigration[i],
+                'Inmigration Men': inmigration_men[i],
+                'Inmigration Women': inmigration_women[i],
+                'Total Inmigration': total_inmigration[i],
             })
 else:
     print("La tabla no se encontró en la página.")
 
 print("Datos extraídos y guardados en el archivo CSV en la ubicación especificada:", csv_filename)
 
-# Conexión con el servidor
+# Creación de la conexión con el servidor
 try:
     connection = mysql.connector.connect(
         host='127.0.0.1',
@@ -77,11 +77,11 @@ try:
     if connection.is_connected():
         print("Conexión exitosa")
 
-    # Crear y eliminar la base de datos
+    # Crear o eliminar la base de datos según tus necesidades
     cursor = connection.cursor()
-    delete_database_query = "DROP DATABASE IF EXISTS emigra"
+    delete_database_query = "DROP DATABASE IF EXISTS inmigra"
     cursor.execute(delete_database_query)
-    create_database_query = "CREATE DATABASE emigra"
+    create_database_query = "CREATE DATABASE inmigra"
     cursor.execute(create_database_query)
     cursor.close()
 
@@ -91,7 +91,7 @@ try:
         port=3306,
         user='root',
         password='root',
-        database='emigra'
+        database='inmigra'
     )
     if connection.is_connected():
         print("Conexión a la base de datos exitosa")
@@ -99,58 +99,57 @@ try:
 except Exception as ex:
     print(ex)
 
-# Cursor para recorrer la base
+# Creación del cursor para recorrer la base
 cursor = connection.cursor()
 
-# Crear tabla si no existe
+# Crear una tabla si no existe
 create_table_query = """
-CREATE TABLE IF NOT EXISTS emigracion (
+CREATE TABLE IF NOT EXISTS inmigracion (
     id INT AUTO_INCREMENT PRIMARY KEY,
     year INT,
-    emigration_men INT,
-    emigration_women INT,
-    total_emigration INT
+    inmigration_men INT,
+    inmigration_women INT,
+    total_inmigration INT
 )
 """
 cursor.execute(create_table_query)
 
 # Insertar datos en la tabla
 insert_query = """
-INSERT INTO emigracion (year, emigration_men, emigration_women, total_emigration)
+INSERT INTO inmigracion (year, inmigration_men, inmigration_women, total_inmigration)
 VALUES (%s, %s, %s, %s)
 """
 
 for i in range(len(years)):
     data = (
         years[i],
-        int(emigration_men[i]),
-        int(emigration_women[i]),
-        int(total_emigration[i]),
+        int(inmigration_men[i]),
+        int(inmigration_women[i]),
+        int(total_inmigration[i]),
     )
     cursor.execute(insert_query, data)
 
 # Commit los cambios
 connection.commit()
 print("Datos insertados en la base de datos SQL.")
-cursor.execute("SELECT * FROM emigracion")
-result1 = cursor.fetchall()
-
+cursor.execute("SELECT * FROM inmigracion")
+res = cursor.fetchall()
 
 # Realiza una consulta SQL
-query = "SELECT id, year, emigration_men, emigration_women, total_emigration FROM emigracion"
+query = "SELECT id, year, inmigration_men, inmigration_women, total_inmigration FROM inmigracion"
 cursor.execute(query)
 
 # Resultados de la consulta
-results = cursor.fetchall()
+res = cursor.fetchall()
 
 # Encabezados de la tabla
 def printo():
-    print("{:<5} {:<8} {:<25} {:<25} {:<25}".format("Id", "Año", "Emigración Hombres", "Emigración Mujeres", "Total Emigración"))
+    print("{:<5} {:<8} {:<25} {:<25} {:<25}".format("Id", "Año", "Inmigración Hombres", "Inmigración Mujeres", "Total Inmigración"))
     print("-" * 90)
     print("-" * 90)
 
 # Imprime tabla con formato desde la base
-    for row in results:
+    for row in res:
         id, year, men, women, total = row
         print("{:<5} {:<8} {:<25} {:<25} {:<25}".format(id, year, men, women, total))
 
@@ -178,82 +177,87 @@ while True:
 
     if opcion == "1":
         # Consulta registro por ID
-        id_a_consultar = int(input("Ingresa el ID del registro que deseas consultar: "))
-        cursor.execute("SELECT * FROM emigracion WHERE id = %s", (id_a_consultar,))
+        consulta_id = int(input("Ingresa el ID del registro que deseas consultar: "))
+        cursor.execute("SELECT * FROM inmigracion WHERE id = %s", (consulta_id,))
         res = cursor.fetchone()
-
+        
         if res:
             print("\nRegistro encontrado:")
-            print("{:<5} {:<8} {:<25} {:<25} {:<25}".format("Id", "Año", "Emigración Hombres", "Emigración Mujeres", "Total Emigración"))
+            print("{:<5} {:<8} {:<25} {:<25} {:<25}".format("Id", "Año", "Inmigración Hombres", "Inmigración Mujeres", "Total Inmigración"))
             print("-" * 90)
             id, year, men, women, total = res
             print("{:<5} {:<8} {:<25} {:<25} {:<25}".format(id, year, men, women, total))
             print("-" * 90)
         else:
-            print(f"No se encontró ningún registro con ID {id_a_consultar}")
+            print(f"No se encontró ningún registro con ID {consulta_id}")
 
     elif opcion == "2":
         # Agrega registro por ID        
         anio_nuevo = int(input("Ingresa el nuevo año: "))
-        emigra_h = int(input("Ingresa la cantidad emigratoria de hombres: "))
-        emigra_w = int(input("Ingresa la cantidad emigratoria de mujeres: "))
+        inmigra_h = int(input("Ingresa la cantidad inmigratoria de hombres: "))
+        inmigra_w = int(input("Ingresa la cantidad inmigratoria de mujeres: "))
 
         insert_query = """
-            INSERT INTO emigracion (year, emigration_men, emigration_women, total_emigration)
+             INSERT INTO inmigracion (year, inmigration_men, inmigration_women, total_inmigration)
             VALUES (%s, %s, %s, %s)
-            """
-        # Calcular el valor de total_emigration
-        total_emigration = emigra_h + emigra_w
+         """
+        # Calcular el valor de total_inmigration
+        total_inmigration = inmigra_h + inmigra_w
 
         data = (
-            
+        
             anio_nuevo,
-            emigra_h,
-            emigra_w,
-            total_emigration,
+            inmigra_h,
+            inmigra_w,
+            total_inmigration,
         )
 
         try:
             cursor.execute(insert_query, data)
             connection.commit()
+            res = cursor.fetchall()
             print("Registro agregado correctamente.")
         except Exception as ex:
             print("Error al agregar el registro:", ex)
+
      
 
     elif opcion == "3":
         # Modifica registro por ID
         id_a_modificar = int(input("Ingresa el ID del registro que deseas modificar: "))
         anio_nuevo = input("Ingresa el nuevo año: ")
-        emigra_h = int(input("Ingresa la nueva cantidad emigratoria de hombres: "))
-        emigra_w = int(input("Ingresa la nueva cantidad emigratoria de mujeres: "))
+        inmigra_h = int(input("Ingresa la nueva cantidad inmigratoria de hombres: "))
+        inmigra_w = int(input("Ingresa la nueva cantidad inmigratoria de mujeres: "))
         # Calcular el valor de total_inmigration
-        total_emigration = emigra_h + emigra_w
+        total_inmigration = inmigra_h + inmigra_w
 
         data = (
         
             anio_nuevo,
-            emigra_h,
-            emigra_w,
-            total_emigration,
+            inmigra_h,
+            inmigra_w,
+            total_inmigration,
         )
-        cursor.execute("UPDATE emigracion SET year = %s, emigration_men = %s, emigration_women = %s, total_emigration = %s WHERE id = %s", (anio_nuevo, emigra_h, emigra_w, total_emigration, id_a_modificar))
+        cursor.execute("UPDATE inmigracion SET year = %s, inmigration_men = %s, inmigration_women = %s, total_inmigration = %s WHERE id = %s", (anio_nuevo, inmigra_h, inmigra_w, total_inmigration, id_a_modificar))
         cursor.fetchone()
-        connection.commit()      
+        connection.commit()   
+
+       
         print("Registro modificado correctamente.")
 
     elif opcion == "4":
         # Eliminar registro por ID
         id_a_eliminar = int(input("Ingresa el ID del registro que deseas eliminar: "))
-        cursor.execute("DELETE FROM emigracion WHERE id = %s", (id_a_eliminar,))
+        cursor.execute("DELETE FROM inmigracion WHERE id = %s", (id_a_eliminar,))
         connection.commit()
         print("Registro eliminado correctamente.")
 
     elif opcion == "5":
-        # Imprimir toda la base de datos ACTUALIZADA.
+       # Imprimir toda la base de datos ACTUALIZADA.
         cursor.execute(query)
-        results = cursor.fetchall()
+        res = cursor.fetchall()
         printo()
+
 
     elif opcion == "6":
         # Salir
@@ -271,4 +275,3 @@ while True:
 # Cierra la conexión
 connection.close()
 print("Conexión a la base cerrada")
-
